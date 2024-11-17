c3db72df01780387fede69dfe8f9aeaa7587a984
Leonardo Albano <leonardoalbanom@yahoo.com.br>
2024-11-17 20:13:38 -0300

    config pesquisa DB

--- src/controllers/questions-controller.ts
+++ src/controllers/questions-controller.ts
@@ -1,11 +1,33 @@
+import { prisma } from "@/prisma"
 import { Request, Response } from "express"
 
 class QuestionsController {
   async index(request: Request, response: Response) {
-    return response.json()
+
+    //Busca
+    const questions = await prisma.question.findMany({
+      where: {
+        title: {
+          contains: request.query.title?.toString().trim(),
+          mode: "insensitive",
+        }
+      },
+    })
+
+    return response.json(questions)
   }
 
   async create(request: Request, response: Response) {
+    const { title, content, user_id } = request.body
+
+    await prisma.question.create({ 
+      data: {
+         title,
+         content,
+         userId: user_id,
+        },
+      })
+
     return response.status(201).json()
   }
 
